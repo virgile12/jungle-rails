d1ab1be2849a7848d8301aa531a56417afb8b32a
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-04-17 20:41:01 +0000

    added admin categories in views/admin and controller/admin

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,41 @@
+class Admin::CategoriesController < ApplicationController
+
+    def index
+      @categories = Category.order(:name).all
+    end
+  
+    def new
+      @category = Category.new
+    end
+  
+    def create
+      @category = Category.new(category_params)
+  
+      if @category.save
+        redirect_to [:admin, :categories], notice: 'category created!'
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      @category = Category.find params[:id]
+      @category.destroy
+      redirect_to [:admin, :categories], notice: 'category deleted!'
+    end
+  
+    private
+  
+    def category_params
+      params.require(:category).permit(
+        :name,
+        :description,
+        :category_id,
+        :quantity,
+        :image,
+        :price
+      )
+    end
+  
+  end
+  
