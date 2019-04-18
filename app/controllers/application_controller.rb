d6abd1cfc8e75f481cfb3a913c5a93b0e6370ffe
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-04-18 22:59:22 +0000

    Completed: added users authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,35 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  
+  #Helper Method for login
+  # protect_from_forgery with: :null_session 
+    
+  # def 
+  #   current_user @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id] 
+  # end 
+  # helper_method :current_user 
+  # def 
+  #   authenticate_user! 
+  #   redirect_to ‘/login’ unless current_user 
+  # end 
+
+    # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+    protect_from_forgery with: :exception
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
+  
+    def authorize
+      redirect_to '/login' unless current_user
+    end
+  
+  
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+
 Rails.application.routes.draw do
-
+  
   root to: 'products#index'
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -11,19 +12,26 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
-
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
